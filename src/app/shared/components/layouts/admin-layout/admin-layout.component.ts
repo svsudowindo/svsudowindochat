f4f41df2de4afcd98360d9225c47ea040dc2c548
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-10-15 01:48:33 +0530

    Implemented Side nav navigations and butons

--- src/app/shared/components/layouts/admin-layout/admin-layout.component.ts
+++ src/app/shared/components/layouts/admin-layout/admin-layout.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.scss']
 })
-export class AdminLayoutComponent implements OnInit {
+export class AdminLayoutComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.openNav();
+  }
+
+  openNav() {
+    document.getElementById('sideNavigation').style.width = '180px';
+    document.getElementById('main').style.marginLeft = '180px';
+  }
+
+  closeNav() {
+    document.getElementById('sideNavigation').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
   }
 
 }
