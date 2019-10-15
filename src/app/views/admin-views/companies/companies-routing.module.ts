59343f4d75badcb09c7284d79e6c6a95efd80721
vipul parmar <vipul.parmar@xcubelabs.com>
2019-10-15 12:17:13 +0530

    companies component created

---
+++ src/app/views/admin-views/companies/companies-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CompaniesComponent } from './companies.component';
+
+const routes: Routes = [
+  {
+  path: '',
+  children: [
+    {
+      path: '',
+      component: CompaniesComponent
+    }
+  ]
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CompaniesRoutingModule { }
