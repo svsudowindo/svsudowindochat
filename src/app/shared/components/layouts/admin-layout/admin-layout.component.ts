7b88e212fea90d167ec706d554f839f5d2e5ba2a
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-10-16 18:38:04 +0530

    Made side nav as overlay

--- src/app/shared/components/layouts/admin-layout/admin-layout.component.ts
+++ src/app/shared/components/layouts/admin-layout/admin-layout.component.ts
@@ -34,20 +34,20 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
     if (this.screenWidth < 576) {
       // very small screens
       document.getElementById('sideNavigation').style.width = '120px';
-      document.getElementById('main').style.marginLeft = '120px';
+      // document.getElementById('main').style.marginLeft = '120px';
     } else if (this.screenWidth >= 576 && this.screenWidth < 768) {
       // small screens
       document.getElementById('sideNavigation').style.width = '300px';
-      document.getElementById('main').style.marginLeft = '300px';
+      // document.getElementById('main').style.marginLeft = '300px';
     } else if (this.screenWidth >= 768 && this.screenWidth < 992) {
       document.getElementById('sideNavigation').style.width = '400px';
-      document.getElementById('main').style.marginLeft = '400px';
+      // document.getElementById('main').style.marginLeft = '400px';
     } else if (this.screenWidth >= 992 && this.screenWidth < 1200) {
       document.getElementById('sideNavigation').style.width = '400px';
-      document.getElementById('main').style.marginLeft = '400px';
+      // document.getElementById('main').style.marginLeft = '400px';
     } else {
       document.getElementById('sideNavigation').style.width = '400px';
-      document.getElementById('main').style.marginLeft = '400px';
+      // document.getElementById('main').style.marginLeft = '400px';
     }
   }
 
