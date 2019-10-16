89a9291325d8baff83a89f5a52ebfa03cd23d9ab
vipul parmar <vipul.parmar@xcubelabs.com>
2019-10-17 01:21:47 +0530

    Created Employees component and employee details component

---
+++ src/app/views/admin-views/employees/employee-details/employee-details.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeeDetailsRoutingModule } from './employee-details-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EmployeeDetailsRoutingModule
+  ]
+})
+export class EmployeeDetailsModule { }
---
+++ src/app/views/admin-views/employees/employees.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeesRoutingModule } from './employees-routing.module';
+import { EmployeesComponent } from './employees.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+
+@NgModule({
+  declarations: [EmployeesComponent, EmployeeDetailsComponent],
+  imports: [
+    CommonModule,
+    EmployeesRoutingModule
+  ]
+})
+export class EmployeesModule { }
