ec7547d84dc37f6ea200a6509971058924e3b54f
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-10-20 23:56:07 +0530

    Implemented add functionality

--- src/app/views/admin-views/profile/educational-details/educational-details.component.ts
+++ src/app/views/admin-views/profile/educational-details/educational-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { qualification } from './educational-details.enums';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-educational-details',
   templateUrl: './educational-details.component.html',
@@ -7,9 +8,31 @@ import { qualification } from './educational-details.enums';
 })
 export class EducationalDetailsComponent implements OnInit {
   qualificationConst = qualification;
-  constructor() { }
+  educationForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.initEducationalForm();
+  }
 
+  initEducationalForm() {
+    this.educationForm = this.formBuilder.group({
+      education: this.formBuilder.array([this.eachEducationForm()])
+    });
+  }
+
+  eachEducationForm(): FormGroup {
+    return this.formBuilder.group({
+      qualification: [''],
+      instituteName: [''],
+      location: [''],
+      state: [''],
+      startDate: [''],
+      endDate: ['']
+    });
+  }
   ngOnInit() {
   }
 
+  addNewQualification() {
+    (this.educationForm.get('education') as FormArray).push(this.eachEducationForm());
+  }
 }
