4c20ef93dc0acb7b4523bff2348446e411149bd5
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-10-22 23:13:19 +0530

    Implmented changes for user dao

--- src/api/controllers/auth/user/user.dao.js
+++ src/api/controllers/auth/user/user.dao.js
@@ -3,13 +3,14 @@ var utils = require('../../../common/services/utils')
 
 exports.createUser = (req, res, next) => {
   let payload = req.body;
-  User.find({ companyID: payload.companyID, email: payload.email }, (err, userslist) => {
+  User.find({ $or:[{ companyID: payload.companyID, email: payload.email }, {companyID: payload.companyID, email: payload.id}]}, (err, userslist) => {
     if (err) {
       console.log('fetch users error');
       return;
     }
     if (userslist.length > 0) {
       console.log('more than one user');
+      res.send('Failed because of more users');
       return;
     }
     var user = new User();
@@ -26,6 +27,7 @@ exports.createUser = (req, res, next) => {
         console.log('user creation failed', err);
       } else {
         console.log('success');
+        res.send('success');
       }
     })
   })
