2e89168ff0ee25ea7fda0b512777095ae24e8341
vipul parmar <vipul.parmar@xcubelabs.com>
2019-10-24 01:47:07 +0530

    created start date and status in registration comany form

---
+++ src/app/views/auth-views/registration/registration.enum.ts
@@ -0,0 +1,33 @@
+export const EMPLOYEEDESIGNATION = [
+    {
+      id: 'ANGULAR_DEVELOPER',
+      label: 'Angular Developer'
+    },
+    {
+      id: 'JAVA_DEVELOPER',
+      label: 'Java Developer'
+    },
+    {
+      id: 'DOT_NET_DEVELOPER',
+      label: '.Net Developer '
+    },
+    {
+      id: 'REACT_DEVELOPER',
+      label: 'React Developer'
+    }
+  ];
+
+  export const STATUS = [
+    {
+      id: 'ACTIVE',
+      label: 'Active'
+    },
+    {
+      id: 'inACTIVE',
+      label: 'In Active'
+    },
+    {
+      id: 'RETIRED',
+      label: 'Retired'
+    }
+  ];
