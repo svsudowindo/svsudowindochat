ccd0d2d96531b9cdb06226c2022fc6246f30e95e
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-11-07 15:23:04 +0530

    Implemented API's for get and set the personal details

--- src/api/routes/admin-routes.js
+++ src/api/routes/admin-routes.js
@@ -3,6 +3,7 @@ var adminRoutes = express.Router();
 
 var companyDAO = require('../controllers/auth/company/company.dao');
 var userDAO = require('../controllers/auth/user/user.dao');
+var personalDetailsDAO = require('../controllers/auth/user/details/personal-details.dao');
 
 adminRoutes.get('/:id/fetch-company-list', companyDAO.getAllCompanies);
 adminRoutes.get('/:id/fetch-employees', userDAO.getAllUsers);
@@ -11,4 +12,6 @@ adminRoutes.get('/:id/getEmployeeByID/:employeeID', userDAO.getEmployeeByID);
 adminRoutes.get('/:id/get-company-details/:companyID', companyDAO.getCompanyById);
 adminRoutes.post('/:id/update-company/:companyID', companyDAO.updateCompany);
 adminRoutes.post('/:id/updateEmployee/:employeeID', userDAO.updateUserAPI);
+adminRoutes.post('/:id/setPersonalDetails/:companyID', personalDetailsDAO.setPersonalDetails);
+adminRoutes.get('/:id/getPersonalDetails/:companyID', personalDetailsDAO.getPersonalDetails);
 exports.adminRoutes = adminRoutes;
