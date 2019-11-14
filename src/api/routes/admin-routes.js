a6c8394fac1b0d328f5cc614b0fd4559a1bbd4cd
svsudowindo <svsudowindo@gmail.com>
2019-11-14 18:51:31 +0530

    Educational and employeement changes

--- src/api/routes/admin-routes.js
+++ src/api/routes/admin-routes.js
@@ -5,6 +5,8 @@ var companyDAO = require('../controllers/auth/company/company.dao');
 var userDAO = require('../controllers/auth/user/user.dao');
 var personalDetailsDAO = require('../controllers/auth/user/details/personal-details.dao');
 var employeementDAO = require('../controllers/auth/user/details/employment-details.dao');
+var educationalDAO = require('../controllers/auth/user/details/educational-details.dao');
+
 adminRoutes.get('/:id/fetch-company-list', companyDAO.getAllCompanies);
 adminRoutes.get('/:id/fetch-employees', userDAO.getAllUsers);
 adminRoutes.post('/:id/reset-password', userDAO.resetPassword);
@@ -16,5 +18,7 @@ adminRoutes.post('/:id/setPersonalDetails/:companyID', personalDetailsDAO.setPer
 adminRoutes.get('/:id/getPersonalDetails/:companyID', personalDetailsDAO.getPersonalDetails);
 adminRoutes.post('/:id/setEmployeementDetails/:companyID', employeementDAO.setEmploymentDetails);
 adminRoutes.get('/:id/getEmployeementDetails/:companyID', employeementDAO.getEmployementDetails);
+adminRoutes.post('/:id/setEducationalDetails/:companyID', educationalDAO.setEducationalDetails);
+adminRoutes.get('/:id/getEducationalDetails/:companyID', educationalDAO.getEducationalDetails);
 
 exports.adminRoutes = adminRoutes;
