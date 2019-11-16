e72ff93722282b15dd9cc620e2958ffbbf3d9145
svsudowindo <svsudowindo@gmail.com>
2019-11-16 23:36:42 +0530

    Fixed SV 12

---
+++ src/app/shared/components/no-records-found/no-records-found.module.ts
@@ -0,0 +1,10 @@
+import { NoRecordsFoundComponent } from './no-records-found.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [NoRecordsFoundComponent],
+  imports: [CommonModule],
+  exports: [NoRecordsFoundComponent]
+})
+export class NoRecordsFoundModule {}
