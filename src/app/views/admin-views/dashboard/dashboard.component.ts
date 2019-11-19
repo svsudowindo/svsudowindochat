d095050756b0f7bf62d01aa2a3be49404810fbc7
svsudowindo <svsudowindo@gmail.com>
2019-11-19 15:57:21 +0530

    Implemented sample upload csv to json object

--- src/app/views/admin-views/dashboard/dashboard.component.ts
+++ src/app/views/admin-views/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -12,4 +13,20 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
   }
 
+  fileChanged(ev) {
+    console.log(ev);
+    let fileReader = new FileReader()
+    fileReader.readAsBinaryString(ev.target.files[0]);
+    fileReader.onload = function (event) {
+      var data = event.target['result'];
+      var workbook = XLSX.read(data, {
+        type: 'binary'
+      });
+      workbook.SheetNames.forEach(function (sheetName) {
+        // Here is your object
+        var XL_row_object = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+        console.log(XL_row_object);
+      })
+    }
+  }
 }
