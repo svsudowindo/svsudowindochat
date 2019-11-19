b51ec437d0d200a648c0a7bf6aa31cea96f85827
vipul parmar <vipul.parmar@xcubelabs.com>
2019-11-19 21:26:33 +0530

    created  opendialog

---
+++ src/app/views/admin-views/employees/employees-bulk-upload/employees-bulk-upload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employees-bulk-upload',
+  templateUrl: './employees-bulk-upload.component.html',
+  styleUrls: ['./employees-bulk-upload.component.scss']
+})
+export class EmployeesBulkUploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/views/admin-views/employees/employees.module.ts
+++ src/app/views/admin-views/employees/employees.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { EmployeesRoutingModule } from './employees-routing.module';
 import { EmployeesComponent } from './employees.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MatDialogModule } from '@angular/material';
+import { EmployeesBulkUploadComponent } from './employees-bulk-upload/employees-bulk-upload.component';
 
 @NgModule({
-  declarations: [EmployeesComponent],
+  declarations: [EmployeesComponent, EmployeesBulkUploadComponent],
   imports: [
     CommonModule,
     EmployeesRoutingModule,
     SharedModule
+  ],
+  entryComponents: [
+    EmployeesBulkUploadComponent
   ]
 })
 export class EmployeesModule { }
