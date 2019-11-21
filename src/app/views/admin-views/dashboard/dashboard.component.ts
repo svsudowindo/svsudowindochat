19a9a808e8943c2c4921c0112ae5cb867f667da6
svsudowindo <svsudowindo@gmail.com>
2019-11-21 14:25:55 +0530

    Implemented Sample download option

--- src/app/views/admin-views/dashboard/dashboard.component.ts
+++ src/app/views/admin-views/dashboard/dashboard.component.ts
@@ -1,6 +1,15 @@
 
 import { Component, OnInit } from '@angular/core';
 import { ConvertExcelToJsonService } from '../../../shared/services/common/convert-excel-to-json/convert-excel-to-json.service';
+import { CommonRequestService } from '../../../shared/services/common-request.service';
+import { RequestEnums } from '../../../shared/constants/request-enums';
+import { StorageService } from '../../../shared/services/storage.service';
+import { LocalStorageEnums } from 'src/app/shared/constants/localstorage-enums';
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -9,7 +18,9 @@ import { ConvertExcelToJsonService } from '../../../shared/services/common/conve
 export class DashboardComponent implements OnInit {
 
   constructor(
-    private excelToJsonService: ConvertExcelToJsonService
+    private excelToJsonService: ConvertExcelToJsonService,
+    private commonRequestService: CommonRequestService,
+    private storageService:StorageService
   ) { }
 
   ngOnInit() {
