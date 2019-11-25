3d82a5d70fad26f3b008ca68a601ac1dca48dcf5
svsudowindo <svsudowindo@gmail.com>
2019-11-26 00:36:41 +0530

    Bulk Upload fixes

--- src/app/shared/components/componentsAsService/popup/popup.service.ts
+++ src/app/shared/components/componentsAsService/popup/popup.service.ts
@@ -1,22 +1,26 @@
-import { Injectable } from '@angular/core';
-import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { PopupComponent } from './popup.component';
-import { PopupInfoService, IDataInfo } from './popup-info.service';
+import { Injectable } from "@angular/core";
+import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
+import { PopupComponent } from "./popup.component";
+import { PopupInfoService, IDataInfo } from "./popup-info.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PopupService {
   modalRef: NgbModalRef;
-  constructor(private _modalService: NgbModal, private _popupinfoService: PopupInfoService) { }
+  constructor(
+    private _modalService: NgbModal,
+    private _popupinfoService: PopupInfoService
+  ) {}
 
   openModal(
     inputData?: IDataInfo,
     backdropInput = true,
     keyboardInput = true,
-    sizeInput: any = 'lg',
-    centeredInput: boolean = false) {
-      this._popupinfoService.data = inputData;
+    sizeInput: any = "lg",
+    centeredInput: boolean = false
+  ) {
+    this._popupinfoService.data = inputData;
 
     this._popupinfoService.modalRef = this._modalService.open(PopupComponent, {
       backdrop: backdropInput,
@@ -24,7 +28,6 @@ export class PopupService {
       size: sizeInput,
       centered: centeredInput
     });
-    console.log(inputData);
     return this._popupinfoService.modalRef.result;
   }
 }
